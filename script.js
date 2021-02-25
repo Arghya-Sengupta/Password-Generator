37fa047b2c7fb95466a160a4a3382d94efecc530
Arghya Sengupta <62299672+Arghya-Sengupta@users.noreply.github.com>
2021-02-25 14:31:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,71 @@ var output = document.getElementById("displayLen");
 output.innerHTML = slider.value;
 var Password = "";
 var pLength = 20;
+color = "#77eb34";
 
 slider.oninput = function()
 {
 	pLength = this.value
 	output.innerHTML = pLength;
+	
+	if(pLength>=20)
+	{
+		color = "#77eb34";
+	}
+	else if(pLength>=8)
+	{
+		color = "#ff6600";
+	}
+	else
+	{
+		color = "#b82905";
+	}
 	Generate();
+}
+
+var N = 4; //Number of boxed checked
+var boxes = document.querySelectorAll(".checkbox");
+for(var i=0;i<4;i++)
+{
+	boxes[i].addEventListener('change', function()
+	{
+		if(this.checked)
+		{
+			Generate();
+			N++;
+			if(N==2)
+			{
+				for(var i=0;i<4;i++)
+					if(boxes[i].checked)
+						boxes[i].disabled = false;
+			}
+		}
+		else
+		{
+			Generate();
+			N--;
+			if(N==1)
+			{
+				for(var i=0;i<4;i++)
+					if(boxes[i].checked)
+						boxes[i].disabled = true;
+			}
+		}
+	});
 }
 
 function Generate()
 {
+	document.getElementById("displayPassword").style.background = color;
 	Password = "";
+	var allowed = "";
 	var lowercase = "abcdefghijklmnopqrstuvwxyz";
 	var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	var numbers = "01234567890123456789";
 	var special = "!@#$%^&*()~_=[]\;',./{}|:<>?-+";
-	var allowed = lowercase;
 	
+	if(document.getElementById("lowercase").checked)
+		allowed += lowercase;
 	if(document.getElementById("uppercase").checked)
 		allowed += uppercase;
 	if(document.getElementById("numbers").checked)
